00faadf68188739b01b82b2a36c4cf3e123c2be2
张飞亚 <2960885715@qq.com>
2025-08-20 17:13:11 +0800

    项目启动

---
+++ src/main/java/com/zfyedu/codehelpe/CodeHelpeApplication.java
@@ -0,0 +1,14 @@
+package com.zfyedu.codehelpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHelpeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeHelpeApplication.class, args);
+        System.out.println("AI助手启动....");
+    }
+
+}
---
+++ src/test/java/com/zfyedu/codehelpe/CodeHelpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zfyedu.codehelpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeHelpeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
